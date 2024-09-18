1cf7b2ef8b19d196e1a6cfca096062680be08b3d
Vish <173876096+Vish-Bh@users.noreply.github.com>
2024-09-18 16:16:47 +0530

    Add files via upload

---
+++ timepass.py
@@ -0,0 +1,46 @@
+import random
+
+dice_art = {
+    1: ("┌─────────┐",
+        "│         │",
+        "│    ●    │",
+        "│         │",
+        "└─────────┘"),
+    2: ("┌─────────┐",
+        "│  ●      │",
+        "│         │",
+        "│      ●  │",
+        "└─────────┘"),
+    3: ("┌─────────┐",
+        "│  ●      │",
+        "│    ●    │",
+        "│      ●  │",
+        "└─────────┘"),
+    4: ("┌─────────┐",
+        "│  ●   ●  │",
+        "│         │",
+        "│  ●   ●  │",
+        "└─────────┘"),
+    5: ("┌─────────┐",
+        "│  ●   ●  │",
+        "│    ●    │",
+        "│  ●   ●  │",
+        "└─────────┘"),
+    6: ("┌─────────┐",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "└─────────┘")
+}
+
+def roll_dice():
+    rolls=[]
+    howmanyrolls=int(input('How many rolls do u want :   '))
+    for i in range(howmanyrolls):
+        rolls.append(random.randint(1, 6))
+    for i in rolls:
+        for lines in dice_art.get(i):
+            print(lines)
+    print(dice_art.get(3))
+    print(rolls)
+roll_dice()
